f93f8674b35281bfa0799cc39f597e32790c78ad
AaronFourie <ST10101067@rccoennect.edu.za>
2024-04-07 22:54:34 +0200

    fix comments length

--- Main.java
+++ Main.java
@@ -2,41 +2,43 @@ import java.util.Arrays;
 public class Main
 {
 	public static void main(String[] args) {
-	    
-//      Original integer array. Modify array values as needed
-	    int[] arr = {0,0,3,4,7,-10,78};
-	    
-//      Set original array as resized array. Modify new array length parameter as needed
-	    arr = resize(arr, 3);               
+//      Int array
+//      Change the values as needed
+	    int[] arr = {0,0,3,4,7,-10,78}; 
+
+//      Set original array as resized array
+//      Change the size parameter as needed 
+	    arr = resize(arr, 3); 
 
 //      Print resized array to console
         System.out.println("Resized Array size: " + arr.length + " | Elements : " + Arrays.toString(arr)); 
 	}
 
-    public static int[] resize(int[] arr, int new_length){
-        if(new_length<=0){                 //Check for edge values
-            return null;                   //Stop method execution by returning null
+    public static int[] resize(int[] arr, int capacity){
+        if(capacity<=0){ //check for edge values
+            return null; 
         }
         
-        int[] temp = new int[new_length];  //Initialize resized array with new length
-        int dif = new_length-arr.length;   //Set difference between resize length and original array length
-        int i;                             //Declare array index val
-        
-        if(dif<=0){                        //Check if difference <= 0
-           for(i=0; i<new_length; i++){
-               temp[i] = arr[i];           //If so, copy over original array until capacity val is reached
+        int[] temp = new int[capacity]; 
+        int i = 0; //val to hold array index
+        int dif = capacity-arr.length; 
+
+        if(dif<=0){
+           for(i=0; i<capacity; i++){
+               temp[i] = arr[i];
            } 
         }
-        else {                             //If difference >0
-            for(i=0; i<arr.length; i++){
-                temp[i] = arr[i];          //Copy over original array until original array length is reached 
-                if(dif>0){                 //If difference is still > 0
-                    temp[i] = 0;           //Assign default val;
-                    dif--;                 //Decrement difference after each of the non copied temp array values is asigned 
+        else {    
+            for(int a=0; a<arr.length; a++){
+                temp[i] = arr[i];
+                i++;
+                if(dif>0){
+                    temp[i] = 0; //asign default val;
+                    dif--;
                 }
             }
         }
-        arr=temp;                          //Set original array as resized array
-        return arr;                        //Return resized array
+        arr=temp;
+        return arr;
     }
 }
