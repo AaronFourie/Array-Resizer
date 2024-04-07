6b60df00f2b54782c2ad5683910ffb689593d390
AaronFourie <ST10101067@rccoennect.edu.za>
2024-04-07 23:10:17 +0200

    fix array indexing

--- Main.java
+++ Main.java
@@ -5,36 +5,41 @@ public class Main
 //      Int array
 //      Change the values as needed
 	    int[] arr = {0,0,3,4,7,-10,78}; 
-
+    
 //      Set original array as resized array
-//      Change the size parameter as needed 
+//      Change the length parameter as needed 
 	    arr = resize(arr, 3); 
 
 //      Print resized array to console
-        System.out.println("Resized Array size: " + arr.length + " | Elements : " + Arrays.toString(arr)); 
+        if(arr==null){
+            System.out.println("Resized Array length cannot be smaller than or equal to 0");
+        }
+        else{
+            System.out.println("Resized Array size: " + arr.length + " | Elements : " + Arrays.toString(arr));
+        }
+         
 	}
 
     public static int[] resize(int[] arr, int capacity){
         if(capacity<=0){ //check for edge values
             return null; 
         }
-        
         int[] temp = new int[capacity]; 
         int i = 0; //val to hold array index
         int dif = capacity-arr.length; 
 
         if(dif<=0){
            for(i=0; i<capacity; i++){
-               temp[i] = arr[i];
+               temp[i] = arr[i]; //copy over original array
            } 
         }
-        else {    
+        else if(dif>0){
             for(int a=0; a<arr.length; a++){
-                temp[i] = arr[i];
+                temp[i] = arr[i]; //coopy over original array
                 i++;
                 if(dif>0){
-                    temp[i] = 0; //asign default val;
-                    dif--;
+                    temp[i] = 0; //asign default val until dif value reaches 0;
+                    dif--; 
                 }
             }
         }
